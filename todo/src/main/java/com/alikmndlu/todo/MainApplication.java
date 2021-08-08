3cbf820925c5d489693ead8db0d62d5d100c2124
Ali Kmndlu <alikmndlu1@gmail.com>
2021-08-08 10:54:57 +0430

    initialize project

---
+++ todo/src/main/java/com/alikmndlu/todo/MainApplication.java
@@ -0,0 +1,9 @@
+package com.alikmndlu.todo;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Starting Application...");
+
+        System.out.println("Shutdown Application...");
+    }
+}
